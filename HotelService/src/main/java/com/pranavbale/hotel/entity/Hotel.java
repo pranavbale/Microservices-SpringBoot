82a31f162e538f7a50f7a69274de689173ada271
Pranav Bale <pranavbale99@gmail.com>
2024-01-19 17:53:32 +0530

    add a changeses in yml and hotel entity

--- HotelService/src/main/java/com/pranavbale/hotel/entity/Hotel.java
+++ HotelService/src/main/java/com/pranavbale/hotel/entity/Hotel.java
@@ -1,2 +1,26 @@
-package com.pranavbale.hotel.entity;public class Hotel {
+package com.pranavbale.hotel.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Hotel {
+
+    @Id
+    @GeneratedValue
+    private UUID hotelId;
+    private String name;
+    private String location;
+    private String about;
 }
