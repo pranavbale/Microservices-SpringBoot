ca95bc1cb2f713baa7be83872089cc08d7bc94b0
Pranav Bale <pranavbale99@gmail.com>
2024-02-04 02:30:48 +0530

    adding a circuit breaker

--- UserService/src/main/java/com/pranavbale/user/service/Controller/UserController.java
+++ UserService/src/main/java/com/pranavbale/user/service/Controller/UserController.java
@@ -2,7 +2,11 @@ package com.pranavbale.user.service.Controller;
 
 import com.pranavbale.user.service.Entity.User;
 import com.pranavbale.user.service.Service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +21,13 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     // get the unique user
+    // in a circuit breaker we add name for configuration
+    // in a circuit breaker we use a fallbackMethod if any service is open then this method call and return type must be same
     @GetMapping("get/{id}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallBack")
     private ResponseEntity<User> getUser(@PathVariable UUID id) {
         return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
     }
@@ -46,6 +55,19 @@ public class UserController {
     private ResponseEntity<String> deleteUser(@PathVariable UUID userId) {
         String msg = userService.deleteUser(userId);
         return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    // rating hotel fall back method for circuit breaker
+    // if any dependent service is open then this function get call
+    public ResponseEntity<User> ratingHotelFallBack(UUID id, Exception ex) {
+        logger.info("Fall Back is executed because service is down : ", ex.getMessage());
+        User user = User.builder()
+                .userId(UUID.randomUUID())
+                .name("Dummy Name")
+                .email("dummyemail@gmail.com")
+                .about("The dependent service is down")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
